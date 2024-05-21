c7d0c64f38ae63ce0ce87f3d44c5f27faf8a5e46
DanielStoev <dannybob63@gmail.com>
2024-05-21 23:26:35 +0300

    Implemented logic to change the lights

--- traffic_lights/src/app/app.component.ts
+++ traffic_lights/src/app/app.component.ts
@@ -1,13 +1,50 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'traffic_lights';
+export class AppComponent implements OnInit {
+  topBottomLight: string = 'green';
+  leftRightLight: string = 'red';
+  private topBottomSequence = ['green', 'yellow', 'red', 'yellow'];
+  private leftRightSequence = ['red', 'yellow', 'green', 'yellow'];
+  private lightTimings = [5000, 2000, 5000, 2000];
+  private topBottomIndex = 0;
+  private leftRightIndex = 0;
+
+  ngOnInit() {
+    this.startTrafficCycle();
+  }
+
+  startTrafficCycle() {
+    this.changeTopBottomLight();
+    this.changeLeftRightLight();
+  }
+
+  changeTopBottomLight() {
+    setTimeout(() => {
+      this.topBottomIndex =
+        (this.topBottomIndex + 1) % this.topBottomSequence.length;
+      this.topBottomLight = this.topBottomSequence[this.topBottomIndex];
+      this.changeTopBottomLight();
+    }, this.lightTimings[this.topBottomIndex]);
+  }
+
+  changeLeftRightLight() {
+    setTimeout(() => {
+      this.leftRightIndex =
+        (this.leftRightIndex + 1) % this.leftRightSequence.length;
+      this.leftRightLight = this.leftRightSequence[this.leftRightIndex];
+      this.changeLeftRightLight();
+    }, this.lightTimings[this.leftRightIndex]);
+  }
+
+  onCrossClick() {
+    if (this.leftRightLight === 'yellow' || this.topBottomLight === 'yellow') {
+      alert('неправилно пресичане');
+    }
+  }
 }
