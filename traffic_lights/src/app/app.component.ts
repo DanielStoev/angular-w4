23f6804f3cde5c78cb83cb0542638c9e5e6d6b04
DanielStoev <dannybob63@gmail.com>
2024-05-22 00:47:12 +0300

    Added 'Emergency' button

--- traffic_lights/src/app/app.component.ts
+++ traffic_lights/src/app/app.component.ts
@@ -9,11 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   topBottomLight: string = 'green';
   leftRightLight: string = 'red';
+  isEmergency: boolean = false;
+  emergencyCooldown: boolean = false;
+  isBlinking: boolean = false;
   private topBottomSequence = ['green', 'yellow', 'red', 'yellow'];
   private leftRightSequence = ['red', 'yellow', 'green', 'yellow'];
   private lightTimings = [5000, 2000, 5000, 2000];
   private topBottomIndex = 0;
   private leftRightIndex = 0;
+  private emergencyBlinkInterval: any;
+  private emergencyTimeout: any;
 
   ngOnInit() {
     this.startTrafficCycle();
@@ -26,25 +31,57 @@ export class AppComponent implements OnInit {
 
   changeTopBottomLight() {
     setTimeout(() => {
-      this.topBottomIndex =
-        (this.topBottomIndex + 1) % this.topBottomSequence.length;
-      this.topBottomLight = this.topBottomSequence[this.topBottomIndex];
-      this.changeTopBottomLight();
+      if (!this.isEmergency) {
+        this.topBottomIndex =
+          (this.topBottomIndex + 1) % this.topBottomSequence.length;
+        this.topBottomLight = this.topBottomSequence[this.topBottomIndex];
+        this.changeTopBottomLight();
+      }
     }, this.lightTimings[this.topBottomIndex]);
   }
 
   changeLeftRightLight() {
     setTimeout(() => {
-      this.leftRightIndex =
-        (this.leftRightIndex + 1) % this.leftRightSequence.length;
-      this.leftRightLight = this.leftRightSequence[this.leftRightIndex];
-      this.changeLeftRightLight();
+      if (!this.isEmergency) {
+        this.leftRightIndex =
+          (this.leftRightIndex + 1) % this.leftRightSequence.length;
+        this.leftRightLight = this.leftRightSequence[this.leftRightIndex];
+        this.changeLeftRightLight();
+      }
     }, this.lightTimings[this.leftRightIndex]);
   }
 
   onCrossClick() {
+    if (this.isEmergency) return;
     if (this.leftRightLight === 'yellow' || this.topBottomLight === 'yellow') {
       alert('неправилно пресичане');
     }
+  }
+
+  activateEmergency() {
+    this.isEmergency = true;
+    
+    this.startBlinking();
+    this.emergencyTimeout = setTimeout(() => {
+      this.stopBlinking();
+      this.isEmergency = false;
+      this.startTrafficCycle();
+      this.emergencyCooldown = true;
+      setTimeout(() => {
+        this.emergencyCooldown = false;
+      }, 10000);
+    }, 10000);
+  }
+
+  startBlinking() {
+    this.isBlinking = true;
+    this.emergencyBlinkInterval = setInterval(() => {
+      this.isBlinking = !this.isBlinking;
+    }, 500);
+  }
+
+  stopBlinking() {
+    clearInterval(this.emergencyBlinkInterval);
+    this.isBlinking = false;
   }
 }
